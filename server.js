c1588a1f14d780bfa51dfe769163b80f4f6a5c2b
Zekun Chen <zekunchen97@gmail.com>
2022-05-28 19:58:34 +0930

    adding the different function

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 const mysql = require('mysql2');
 const inquirer = require("inquirer");
 const table = require("console.table");
-const { start } = require('repl');
 
 const db = mysql.createConnection(
     {
@@ -32,20 +31,24 @@ const db = mysql.createConnection(
         message: "What would you like to do?",
         name: "option"
       })
-      .then(function(result) {
-        console.log("You entered: " + result.option);
+      .then(function(res) {
+        console.log("You entered: " + res.option);
   
         switch (result.option) {
           case "Add department":
+            //done
             addDepartment();
             break;
           case "Add role":
+            //done
             addRole();
             break;
           case "Add employee":
+            //done
             addEmployee();
             break;
           case "View departments":
+            //done
             viewDepartment();
             break;
           case "View roles":
@@ -73,8 +76,75 @@ function addDepartment() {
       db.query('INSERT INTO department (name) VALUES (?)', [res.department], function(err, data) {
         if (err) throw err;
         console.table("Successfully Inserted");
+        Start();
     })
     })
 }
+
+function addEmployee(){
+  inquirer.prompt([{
+    type: "input",
+            name: "firstName",
+            message: "What is the employees first name?"
+        },
+        {
+            type: "input",
+            name: "lastName",
+            message: "What is the employees last name?"
+        },
+        {
+            type: "number",
+            name: "roleId",
+            message: "What is the employees role ID"
+        },
+        {
+            type: "number",
+            name: "managerId",
+            message: "What is the employees manager's ID?"
+
+  }]).then(function(res){
+    db.query('INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES (?, ?, ?, ?)', [res.firstName, res.lastName, res.roleId, res.managerId], function(err, data) {
+      if (err) throw err;
+      console.table("Successfully Inserted");
+      Start();
+    })
+    })
+  }
+  
+ function addRole(){
+  inquirer.prompt([
+    {
+        message: "enter title:",
+        type: "input",
+        name: "title"
+    }, {
+        message: "enter salary:",
+        type: "number",
+        name: "salary"
+    }, {
+        message: "enter department ID:",
+        type: "number",
+        name: "department_id"
+    }
+]).then(function (res) {
+    connection.query("INSERT INTO roles (title, salary, department_id) values (?, ?, ?)", [res.title, res.salary, res.department_id], function (err, data) {
+      if (err) throw err;
+      console.table(data);
+    })
+    Start();
+})
+
+}
+
+function viewDepartment(){
+  connection.query("SELECT * FROM employee", function (err, data) {
+    console.table(data);
+  })
+}
+
+
+ 
+
+
 
 Start()
